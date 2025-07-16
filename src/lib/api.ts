2efa35357e66a73373ac66a8a11bc171e20025b1
Mayank Sharma <22bcs10182@cuchd.in>
2025-07-16 12:54:30 +0530

    api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import toast from 'react-hot-toast'
 import type { ApiResponse } from '../types'
 
-const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://localhost/api/v1'
+const API_BASE_URL = import.meta.env.VITE_API_URL 
 // const API_BASE_URL = import.meta.env.VITE_API_URL || 'http://57.159.27.100:8080/api/v1'
 
 const apiClient = axios.create({
