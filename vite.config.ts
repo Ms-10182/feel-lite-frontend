1791af2a40361b990d2f8668ff95001dbf7b7a43
Mayank Sharma <22bcs10182@cuchd.in>
2025-07-16 14:42:49 +0530

    added vercel confing

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   server: {
     port: 5173,
     proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
+      "/api": {
+        target: "http://localhost:8080",
         // target: 'http://57.159.27.100:8080',
         changeOrigin: true,
-        secure: false,           // if your backend is self-signed HTTPS
-        rewrite: path => path.replace(/^\/api/, ''), // optional: strip `/api`
+        secure: false, // if your backend is self-signed HTTPS
+        rewrite: (path) => path.replace(/^\/api/, ""), // optional: strip `/api`
       },
     },
   },
+  base: "/",
   build: {
-    outDir: 'dist', // ensures output goes to dist/
-    sourcemap: true // optional for debugging in production
-  }
-})
+    outDir: "dist", // ensures output goes to dist/
+    sourcemap: true, // optional for debugging in production
+  },
+});
